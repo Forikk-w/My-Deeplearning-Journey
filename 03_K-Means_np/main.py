4e999b5d21f34cb0b20cbaa5bf1c7097e4ad567b
Forikk-w <diwu61474@gmail.com>
2026-02-07 14:40:39 +0800

    feat: prepare for SVM and K-Means algorithms

---
+++ 03_K-Means_np/main.py
@@ -0,0 +1,8 @@
+
+def run() :
+    pass
+if __name__ == '__main__':
+    print("03 K-Means by numpy")
+    print(f"{'-' * 25}")
+
+    run()
---
+++ 04_SVM_np/main.py
@@ -0,0 +1,8 @@
+
+def run() :
+    pass
+if __name__ == '__main__':
+    print("04 SVM by numpy")
+    print(f"{'-' * 25}")
+
+    run()
